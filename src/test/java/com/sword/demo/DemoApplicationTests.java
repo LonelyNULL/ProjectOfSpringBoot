d613cc179f3c2c2f531c5fe029d66b67dfdbeeb2
sword <wkj19920710@163.com>
2020-06-22 15:20:54 +0800

    基于SpringBoot的项目初始化

---
+++ src/test/java/com/sword/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sword.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
